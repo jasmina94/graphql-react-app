f44a05924c701fd0319a5712ebc48d14281f459d
Jasmina <jasmina.eminovski@gmail.com>
2021-09-13 16:42:32 +0200

    Almost finished

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-const express = require('express')
-const { graphqlHTTP } = require('express-graphql')
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const path = require('path');
+const cors = require('cors');
+
 const schema = require('./schema');
 
 const app = express();
 
+app.use(cors());
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+})
 
 const PORT = process.env.PORT || 5000;
 
